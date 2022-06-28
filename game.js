64774a32b9e4166183cd888a60acba61ad3e28fd
Anders Windelhed <anders.windelhed@tv4.se>
2022-06-28 19:08:03 +0200

    Setting width and height of canvas

--- game.js
+++ game.js
@@ -1,10 +1,19 @@
+
 const setup = () => {
-  var c = document.getElementById('canvas')
-  var ctx = c.getContext('2d')
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
+
+  const width = document.body.clientWidth
+  const height = document.body.clientHeight
+
+  canvas.width = width
+  canvas.height = height
+
+  ctx.fillStyle = 'rgb(5, 66, 135)';
+  ctx.fillRect(0, 0, width, height)
+
   ctx.moveTo(0, 0)
-  ctx.lineTo(200, 100)
+  ctx.lineTo(width, height)
   ctx.stroke()
 }
 setup()
-
-console.log('asdf')
